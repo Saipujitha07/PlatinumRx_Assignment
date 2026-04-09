71a154c66c4047381cdb294d8d262d71806850e1
Saipujitha07 <121871152+Saipujitha07@users.noreply.github.com>
2026-04-09 15:31:25 +0530

    Add files via upload

--- Python/01_Time_Converter.py
+++ Python/01_Time_Converter.py
@@ -1,1 +1,12 @@
+def convert_minutes(minutes):
+    if minutes < 0:
+        return "Invalid input (minutes cannot be negative)"
 
+    hours = minutes // 60
+    remaining = minutes % 60
+    return f"{hours} hrs {remaining} minutes"
+
+minutes = int(input("Enter total minutes: "))
+result = convert_minutes(minutes)
+
+print(result)
